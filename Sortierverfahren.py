65d3d457e1cefe3a5021b4157b567dba0069f487
janheuer <jan.heuer@me.com>
2017-06-11 19:07:44 +0200

    Create Sortierverfahren.py

--- Sortierverfahren.py
+++ Sortierverfahren.py
@@ -2,39 +2,38 @@
 # Hausaufgabe 2 - Sortierverfahren
 # Abgabedatum: 11.6.2017
 #
-# Gruppennummer: <Bitte eintragen>
+# Gruppennummer: 55
 # Gruppenmitglieder:
-# - <Bitte eintragen>
-# - <Bitte eintragen>
-# - <Bitte eintragen>
+# - Jan Heuer
+# - Max Wiedenhöft
+# - Eike Olaf Pubantz
 
-# Implementieren Sie ab hier Ihre Lösungen:
 def sortMerge(a):
-    print('Parameter a:', a)
+    print('Parameter a: ', a)
 
-    if len(a) == 1:
+    if len(a) == 1: #Basisfall
         return a
     else:
-        n = len(a)//2
-        l = sortMerge(a[0:n])
-        r = sortMerge(a[n:len(a)])
-        result = merge(l, r)
+        n = len(a)//2 #Mitte der Liste
+        l = sortMerge(a[0:n]) #Sortieren der linken Teilliste
+        r = sortMerge(a[n:len(a)]) #Sortieren der rechten Teilliste
+        result = merge(l, r) #Zusammenführen der sortierten Teillisten
         print("Zwischenergebnis: ", result)
     return result
 
 def merge(a, b):
     result = []
-    while (len(a) != 0) and (len(b) != 0):
-        if a[0] < b[0]:
+    while (len(a) != 0) and (len(b) != 0): #Zusammenführen bis eine Liste leer
+        if a[0] < b[0]: #Anhängen des kleineren der beiden Elemente an Ergebnis
             result.append(a[0])
-            a = a[1:len(a)]
+            a = a[1:len(a)] #Erstes Element von a entfernen
         else:
             result.append(b[0])
-            b = b[1:len(b)]
-    if a != []:
-        result = result + a
-    if b != []:
-        result = result + b
+            b = b[1:len(b)] #Erstes Element von b entfernen
+    if a != []: #Falls a noch nicht leer
+        result = result + a #Anhängen von a an Ergebnis
+    if b != []: #Falls b noch nicht leer
+        result = result + b #Anhängen von b an Ergebnis
     return result
 
 def sortHeapDesc(a):
