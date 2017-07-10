87299e20c8bf7bdd1bb0583b2943501286b6ed96
janheuer <jan.heuer@me.com>
2017-07-10 19:28:19 +0200

    Add files via upload

---
+++ binom.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+## Algorithmen und Datenstrukturen
+## Hausaufgabe 3 - Dynamische Programmierung
+## Abgabedatum: 12.7.2017
+##
+## Gruppennummer: 55
+## Gruppenmitglieder:
+## - Jan Heuer
+## - Max Wiedenhöft
+## - Eike Olaf Pubantz
+
+## ACHTUNG: Verwenden Sie für Ihre Kommentare nur ein einzelnes #-Zeichen:
+# so sollte Ihr Kommentar markiert werden
+## so sind die Kommentare aus der Aufgabenstellung markiert.
+
+## Implementieren Sie ab hier Ihre Lösungen:
+def dynCompNChooseK(n, k):
+    print('Parameter n:') ## Als Ausgabe genügt es nicht, nur die Parameter und das Ergebnis auszugeben.
+    print(n)
+    print('Parameter k:')
+    print(k)
+    ## hier soll Ihre Implementierung stehen.
+    table = dynCompNChooseKTable(n, k)
+    result = table[n][k]
+    return result
+
+def dynCompNChooseKTable(n, k):
+    #print('Parameter n:')
+    #print(n)
+    #print('Parameter k:')
+    #print(k)
+    ## hier soll Ihre Implementierung stehen.
+    result = []
+    for i in range(0,n+1):
+        result.append([])
+        for j in range(0,k+1):
+            result[i].append(0)
+    for i in range(0,n+1):
+        result[i][0] = 1
+    for i in range(1,n+1):
+        for j in range(1,k+1):
+            result[i][j] = result[i-1][j] + result[i-1][j-1]
+    return result
+
+## Hier ist ein Testfall:
+n = 5
+k = 3
+
+n = int(input("n = "))
+k = int(input("k = "))
+
+result = dynCompNChooseK(n, k)
+resultTable = dynCompNChooseKTable(n, k)
+
+print('berechnet:')
+print(result) ## 10
+print('mit der Tabelle:')
+print(resultTable)
+##  #k
+##[ #0  1  2  3  4  5
+##  [1, 0, 0, 0, 0, 0], #n=0
+##  [1, 1, 0, 0, 0, 0], #n=1
+##  [1, 2, 1, 0, 0, 0], #n=2
+##  [1, 3, 3, 1, 0, 0], #n=3
+##  [1, 4, 6, 4, 1, 0], #n=4
+##  [1, 5, 10,10,5, 1]  #n=5
+##[
