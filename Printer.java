70462155c9a74f98b9f5b6e759c908e7bdcbbc2c
janheuer <jan.heuer@me.com>
2017-07-23 15:49:02 +0200

    Update Printer.java

--- Printer.java
+++ Printer.java
@@ -1,3 +1,14 @@
+/* 	Praxis der Programmierung
+	Hausaufgabe 2
+	Abgabedatum: 23.07.2017
+
+	Gruppennummer: 64
+	Gruppenmitglieder:
+	- Eike Olaf Pubantz
+	- Max Wiedenhöft
+	- Jan Heuer
+*/
+
 public class Printer<T> implements Visitor<T>{
 
   public void process(Node<T> node){
