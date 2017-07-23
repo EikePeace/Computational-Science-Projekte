b6311c7093ac6c5d4ecc396f93d0112dc9f6b1c0
janheuer <jan.heuer@me.com>
2017-07-23 15:48:03 +0200

    Add files via upload

---
+++ Pass.java
@@ -0,0 +1,16 @@
+/* 	Praxis der Programmierung
+	Hausaufgabe 2
+	Abgabedatum: 23.07.2017
+
+	Gruppennummer: 64
+	Gruppenmitglieder:
+	- Eike Olaf Pubantz
+	- Max Wiedenhöft
+	- Jan Heuer
+*/
+
+public class Pass<T> implements Visitor<T>{
+
+  public void process(Node<T> node){}
+
+}
