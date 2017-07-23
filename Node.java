6209d61372fab5ea552a75978b07db89db9cad39
janheuer <jan.heuer@me.com>
2017-07-23 15:47:27 +0200

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,3 +1,14 @@
+/* 	Praxis der Programmierung
+	Hausaufgabe 2
+	Abgabedatum: 23.07.2017
+
+	Gruppennummer: 64
+	Gruppenmitglieder:
+	- Eike Olaf Pubantz
+	- Max Wiedenhöft
+	- Jan Heuer
+*/
+
 import java.util.ArrayList;
 
 public class Node<T>{
@@ -42,33 +53,34 @@ public class Node<T>{
     }
   }
 
-  public void visitAll(Visitor<T> v){
-    ArrayList<Node> queue = new ArrayList<Node>();
-    ArrayList<Node> visited = new ArrayList<Node>();
-    queue.add(this);
-    Node curNode;
-    while (!queue.isEmpty()){
-      curNode = queue.remove(0);
-      v.process(curNode);
-      visited.add(curNode);
-      for (int i = 0 ; i < curNode.adj.size() ; i++){
-        if (!visited.contains(curNode.adj.get(i))){
-          if (!queue.contains(curNode.adj.get(i))){
-            queue.add((Node)curNode.adj.get(i));
+  public ArrayList<Node> visitAll(Visitor<T> v){
+    ArrayList<Node> queue = new ArrayList<Node>(); // Warteschlange
+    ArrayList<Node> visited = new ArrayList<Node>(); // Speichert bereits besuchte Knoten
+    queue.add(this); // Startknoten in Warteschlange
+    Node curNode; // Wird aktuellen Knoten speichern
+    while (!queue.isEmpty()){ // Solange noch Knoten in Warteschlange
+      curNode = queue.remove(0); // Ersten Knoten der Warteschlange entnehmen
+      v.process(curNode); // Visitor process für aktuellen Knoten aufrufen
+      visited.add(curNode); // Aktuellen Knoten zur besucht Liste hinzufügen
+      for (int i = 0 ; i < curNode.adj.size() ; i++){ // Durchlaufen der Adjazenzliste des aktullen Knoten
+        if (!visited.contains(curNode.adj.get(i))){ // Knoten noch nicht besucht
+          if (!queue.contains(curNode.adj.get(i))){ // Knoten noch nicht in Warteschlange
+            queue.add((Node)curNode.adj.get(i)); // Knoten zur Warteschlange hinzufügen
           }
         }
       }
     }
+    return visited; // Zurückgeben aller besuchter Knoten
   }
 
   boolean isConnected(Node<T> node){
-    for (int i = 0; i < this.adj.size(); i++){
-		if (this.adj.get(i) == node){
-		  return true;
-		}
-		else {
-		return false;
-		}
-	}
+    // Ausrufen von visitAll mit Visitor-Implementierung Pass (macht nichts)
+    // visited speichert dann alle Knoten die erreichbar sind
+    ArrayList<Node> visited = this.visitAll(new Pass<T>());
+    if (visited.contains(node)){ // Falls gesuchter Knoten in visited
+      return true;
+    }
+    return false;
   }
+
 }
